3a0636de9aba1a398b2e065ccf97b75abd4435ae
Ab-Romia <aabouroumia@gmail.com>
2024-12-22 18:31:09 +0200

    del

--- sql/DML/populate_customers.sql
+++
@@ -1,8 +0,0 @@
-USE car_rental_system;
-
-INSERT INTO Customer (FirstName, LastName, Email, Phone, Address, AccountCreatedDate)
-VALUES
-    ('John', 'Doe', 'john.doe@example.com', '123-456-7890', '123 Elm St, City Center', '2024-01-01'),
-    ('Jane', 'Smith', 'jane.smith@example.com', '987-654-3210', '456 Pine St, Suburbia', '2024-02-15'),
-    ('Alice', 'Brown', 'alice.brown@example.com', '555-123-4567', '789 Maple Ave, Uptown', '2024-03-10'),
-    ('Bob', 'Johnson', 'bob.johnson@example.com', '222-333-4444', '321 Oak Rd, Countryside', '2024-04-20');
