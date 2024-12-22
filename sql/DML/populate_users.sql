fe5de806f7281430944679195380cd73b13b3401
Ab-Romia <aabouroumia@gmail.com>
2024-12-22 18:31:09 +0200

    del

--- sql/DML/populate_users.sql
+++
@@ -1,7 +0,0 @@
-USE car_rental_system;
-
-INSERT INTO users (email, username, password, created_at) VALUES
-                                                              ('romia@team.com', 'romia', 'hashed_password_1', NOW()),
-                                                              ('john.doe@example.com', 'johndoe', 'hashed_password_1', NOW()),
-                                                              ('jane.smith@example.com', 'janesmith', 'hashed_password_2', NOW()),
-                                                              ('alice.jones@example.com', 'alicejones', 'hashed_password_3', NOW());
