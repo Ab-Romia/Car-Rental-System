17d5bffb3ccd4d0061f5cc2edf203b94a9dab387
MohabG2 <es-mohab.sherif2026@alexu.edu.eg>
2024-12-29 21:44:32 +0200

    Ready for routing

--- car-rental-frontend/src/app/app-routing.module.ts
+++ car-rental-frontend/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  // Define your routes here
+  // Add your routes here
 ];
 
 @NgModule({
